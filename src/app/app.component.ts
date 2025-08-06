f368cedeec34c0cc8a38c71eb718000ce48e8efe
carole pigna <carolepigna@gmail.com>
2025-08-06 21:52:43 +0200

    modification component Parcours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { MonhistoireComponent } from './monhistoire/monhistoire.component';
+import { ParcoursComponent } from './parcours/parcours.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+
+    NavbarComponent,
+    HeaderComponent,
+    MonhistoireComponent,
+    ParcoursComponent,
+    ProjetsComponent,
+    BlogComponent,
+    ContactComponent
+  ],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'mon-portfolio';
-}
+export class AppComponent {}
